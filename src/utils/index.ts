12da494ae34f9799011e3259a23cc15713412817
DirkDigglerz <liamsdargie@gmail.com>
2025-11-04 11:49:56 +0000

    feat: Export map utility from utils index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,7 @@ export * from "./fetchNui";
 export * from "./internalEvent";
 export * from "./locales";
 export * from "./misc";
+export * from "./map";
 export * from "./profanity";
 export * from "./createSkill";
 export * from "./images";
